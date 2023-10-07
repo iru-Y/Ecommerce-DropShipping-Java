02bfc3ab2b8b64e2da939b1337f4931ef23fc450
iruy <yuri_matteus@hotmail.com>
2023-10-07 15:34:28 -0300

    Fazendo o swagger

--- src/main/java/com/delivery/trizi/trizi/controllers/AuthController.java
+++ src/main/java/com/delivery/trizi/trizi/controllers/AuthController.java
@@ -5,6 +5,10 @@ import com.delivery.trizi.trizi.infra.security.jwtUtils.RoleMailDto;
 import com.delivery.trizi.trizi.domain.user.UserModel;
 import com.delivery.trizi.trizi.infra.security.jwtUtils.TokenService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +20,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "auth")
 @AllArgsConstructor
+@Tag(name = "Autenticação", description = "Endpoints relacionados à autenticação")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
     @PostMapping(value = "/mail")
+    @Operation(summary = "Autenticação por e-mail e senha", description = "Realiza a autenticação do usuário utilizando e-mail e senha," +
+            " aonde o retorno é um token JWT")
     public ResponseEntity mail(@RequestBody RoleMailDto data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.mail(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
--- src/main/java/com/delivery/trizi/trizi/controllers/UserController.java
+++ src/main/java/com/delivery/trizi/trizi/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.delivery.trizi.trizi.controllers;
 import com.delivery.trizi.trizi.domain.user.UserDto;
 import com.delivery.trizi.trizi.domain.user.UserModel;
 import com.delivery.trizi.trizi.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
@@ -18,17 +20,23 @@ import java.util.List;
 @RestController
 @RequestMapping("users")
 @AllArgsConstructor
+@Tag(name = "Área relacionado aos usuários",
+        description = "As informações sobre credenciais também estão envolvidas neste end-point")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping
+    @Operation(summary = "Retorno páginado de todos os usuários do sistema",
+            description = "Você pode usar os parâmetros ?page={qual a página que você quer navegar}&size={tamanho da lista}")
     public ResponseEntity<List<UserModel>> getAllUsers() {
         List<UserModel> users = userService.getAll();
         return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/id/{id}")
+    @Operation(summary = "Retorna o usuário através do email",
+            description = "Aqui você busca por id")
     public ResponseEntity<UserModel> getByUserID(@PathVariable String id) {
         UserModel user = userService.getById(id);
         if (user != null) {
@@ -42,6 +50,8 @@ public class UserController {
     }
 
     @PostMapping()
+    @Operation(summary = "Aqui você cria um usuário, sem foto de perfil",
+            description = "para adicionar uma foto de perfil, utilize a operação Patch")
     public ResponseEntity<UserModel> post(@RequestBody UserDto userDto
                                           ) {
         UserModel user = new UserModel();
@@ -53,6 +63,8 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Aqui você atualiza os dados do usuário, assim como passa a foto de seu perfil",
+            description = "Use o parâmetro ou form-data 'user' : '{json do user} | 'file': {faça upload da sua foto aqui}")
     public ResponseEntity<UserModel> updateUser(@PathVariable String id,
                                                 @RequestParam("user") UserDto userDto,
                                                 @RequestParam("file") MultipartFile file) {
@@ -63,6 +75,8 @@ public class UserController {
     }
 
     @PatchMapping(value = "/image/{mail}")
+    @Operation(summary = "Aqui você atualiza os dados do usuário pelo email",
+            description = "Use o parâmetro 'file': {faça upload da sua foto aqui}, além de passar o /{mail}")
     public ResponseEntity<UserModel> updateProfImg (@RequestParam (value = "file") MultipartFile file,
                                                     @PathVariable String mail) {
         UserModel updatedUser = userService.put(mail, file);
@@ -70,18 +84,23 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Aqui você deleta um usuário")
     public ResponseEntity<String> deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
         return ResponseEntity.ok().body("Usuário excluído com sucesso.");
     }
 
     @GetMapping("/mail/{mail}")
+    @Operation(summary = "Aqui você pega todos os usuários pelo email",
+            description = "passar o /{mail}, ele irá retornar um usuário através do email")
     public ResponseEntity<UserModel> getByMail(@PathVariable String mail) {
         UserModel user = userService.findByMail(mail);
             return ResponseEntity.ok().body(user);
     }
     @PatchMapping("/favorites")
-    public UserModel favorites (@RequestParam String mail, @RequestParam String description) {
+    @Operation(summary = "Aqui você adiciona um produto favorito ao seu usuário",
+            description = "passe um mail e uma descrição do produto para adicionar, ex: /user/favorites?m=abcdef@hotmail.com&d=pasta de dentes. Isso anexará um produto favorito ao seu usuário")
+    public UserModel favorites (@RequestParam (value = "m") String mail, @RequestParam (value = "d") String description) {
         var user = userService.findByMail(mail);
         return userService.favorites(user, description);
     }
